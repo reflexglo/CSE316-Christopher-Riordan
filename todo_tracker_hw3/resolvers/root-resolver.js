a2c09d6bd6b331488058d4d94124c96894be86b5
reflexglo <theicycrew@gmail.com>
2021-04-04 16:53:13 -0400

    Finished program

--- todo_tracker_hw3/resolvers/root-resolver.js
+++
@@ -1,4 +0,0 @@
-const userResolvers = require('./user-resolvers');
-const todolistResolvers = require('./todolist-resolvers');
-
-module.exports = [userResolvers, todolistResolvers];
--- todo_tracker_hw3/typedefs/readme.md
+++
@@ -1,1 +0,0 @@
-This folder should contain necessary graphql typedefs. 
